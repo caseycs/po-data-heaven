760769b248cf7a988667e9ee1b9d9bf89b9caa13
Ilia Kondrashov <caseycs@gmail.com>
2015-10-26 22:28:29 +0100

    better rotate transformer

--- app/Exception/ColumnNotFoundException.php
+++ app/Exception/ColumnNotFoundException.php
@@ -3,4 +3,8 @@ namespace PODataHeaven\Exception;
 
 class ColumnNotFoundException extends PODataHeavenException
 {
+    public function __construct($column)
+    {
+        parent::__construct('Required column not found: ' . $column);
+    }
 }
